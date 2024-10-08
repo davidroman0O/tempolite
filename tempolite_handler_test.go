8cda28fd8e55918870acda630991e2e5bf59ef3c
David Roman <romandavid83@gmail.com>
2024-10-08 17:31:26 -0400

    uh oh

--- tempolite_handler_test.go
+++ tempolite_handler_test.go
@@ -265,3 +265,92 @@ func TestHandlerRetriesParent(t *testing.T) {
 		return ti.IsCompleted()
 	}, time.Second)
 }
+
+func dynamicHandlerChildrenHandler(ctx HandlerContext, task TaskInt) (interface{}, error) {
+	log.Printf("handlerChildrenHandler Task %d", task)
+	return nil, nil
+}
+
+func dynamicHandlerSecondChildrenHandler(ctx HandlerContext, task TaskInt) (interface{}, error) {
+	log.Printf("handlerChildrenHandler Task %d", task)
+	return nil, nil
+}
+
+func dynamicHandlerParentRetry(ctx HandlerContext, task TaskInt) (interface{}, error) {
+	var err error
+	var id string
+
+	if retryFlag.Load() {
+		if id, err = ctx.Enqueue(dynamicHandlerSecondChildrenHandler, TaskInt{task.Data + 2}); err != nil {
+			return nil, err
+		}
+		if _, err = ctx.WaitFor(id); err != nil {
+			return nil, err
+		}
+	}
+
+	if id, err = ctx.Enqueue(dynamicHandlerChildrenHandler, TaskInt{task.Data + 2}); err != nil {
+		return nil, err
+	}
+
+	if !retryFlag.Load() {
+		retryFlag.Store(true)
+		fmt.Println("retryFlag set to false")
+		return nil, fmt.Errorf("failied on purpose")
+	}
+
+	log.Printf("parentHandler Task %d", task)
+
+	return ctx.WaitFor(id)
+}
+
+// go test -v -timeout 30s -run ^TestHandlerDynamicRetriesParent$ .
+func TestHandlerDynamicRetriesParent(t *testing.T) {
+
+	ctx := context.Background()
+
+	tp, err := New(ctx,
+		WithDestructive(),
+		WithPath("./db/tempolite_dynamic_handler_parent_retries_test.db"),
+	)
+	if err != nil {
+		t.Fatalf("Error creating Tempolite: %v", err)
+	}
+	defer tp.Close()
+
+	retryFlag.Store(false)
+
+	if err := tp.RegisterHandler(dynamicHandlerChildrenHandler); err != nil {
+		t.Fatalf("Error registering handler: %v", err)
+	}
+	if err := tp.RegisterHandler(dynamicHandlerSecondChildrenHandler); err != nil {
+		t.Fatalf("Error registering handler: %v", err)
+	}
+
+	if err := tp.RegisterHandler(dynamicHandlerParentRetry); err != nil {
+		t.Fatalf("Error registering handler: %v", err)
+	}
+
+	var id string
+	if id, err = tp.Enqueue(ctx, dynamicHandlerParentRetry, TaskInt{1}); err != nil {
+		t.Fatalf("Error enqueuing task: %v", err)
+	}
+
+	log.Printf("ID: %s", id)
+
+	ctxTimeout := context.Background()
+	ctxTimeout, cancel := context.WithTimeout(ctxTimeout, 5*time.Second)
+	defer cancel()
+
+	data, err := tp.WaitFor(ctxTimeout, id)
+	if err != nil {
+		t.Fatalf("Error waiting for task: %v", err)
+	}
+
+	log.Printf("Data: %v", data)
+
+	tp.Wait(func(ti TempoliteInfo) bool {
+		log.Println(ti)
+		return ti.IsCompleted()
+	}, time.Second)
+}
