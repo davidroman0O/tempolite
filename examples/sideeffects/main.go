b491c6911a9bc5e8d19c903fe4445c5db9fa2e34
David Roman <romandavid83@gmail.com>
2024-10-13 15:00:48 -0400

    ex

---
+++ examples/sideeffects/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/davidroman0O/go-tempolite"
+)
+
+func Workflow(ctx tempolite.WorkflowContext[string]) error {
+
+	var pathAOrB bool
+	if err := ctx.SideEffect("switch", func(ctx tempolite.SideEffectContext[string]) bool {
+		return true
+	}).Get(&pathAOrB); err != nil {
+		return err
+	}
+
+	if pathAOrB {
+		if err := ctx.ActivityFunc("activityA", ActivityA).Get(); err != nil {
+			return err
+		}
+	} else {
+		if err := ctx.ActivityFunc("activityb", ActivityB).Get(); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func ActivityA(ctx tempolite.ActivityContext[string]) error {
+	fmt.Println("Activity A")
+	return nil
+}
+
+func ActivityB(ctx tempolite.ActivityContext[string]) error {
+	fmt.Println("Activity B")
+	return nil
+}
+
+func main() {
+	ctx := context.Background()
+	tp, err := tempolite.New[string](ctx)
+	if err != nil {
+		log.Fatalf("Failed to create Tempolite instance: %v", err)
+	}
+
+	if err := tp.RegisterActivityFunc(ActivityA); err != nil {
+		log.Fatalf("Failed to register activity: %v", err)
+	}
+
+	if err := tp.RegisterActivityFunc(ActivityB); err != nil {
+		log.Fatalf("Failed to register activity: %v", err)
+	}
+
+	if err := tp.RegisterWorkflow(Workflow); err != nil {
+		log.Fatalf("Failed to register workflow: %v", err)
+	}
+
+	if err := tp.Workflow("workflow", Workflow).Get(); err != nil {
+		log.Fatalf("Failed to enqueue workflow: %v", err)
+	}
+
+	if err := tp.Wait(); err != nil {
+		log.Fatalf("Failed to wait for Tempolite instance: %v", err)
+	}
+
+	tp.Close()
+}
--- examples/versioning/main.go
+++ examples/versioning/main.go
@@ -1,5 +1,3 @@
-// File: ./examples/versioning/main.go
-
 package main
 
 import (
