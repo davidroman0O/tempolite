4ea65c6fdfe84702363abc8c554f75183c418d80
David Roman <romandavid83@gmail.com>
2024-10-12 22:44:59 -0400

    Update main.go

--- examples/versioning/main.go
+++ examples/versioning/main.go
@@ -6,6 +6,8 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"sync/atomic"
+	"time"
 
 	"github.com/davidroman0O/go-tempolite"
 )
@@ -21,9 +23,28 @@ const (
 	ComputeTaxes = "ComputeTaxes"
 )
 
+var codeAfterUpdateOneRuntime atomic.Bool
+var codeAfterUpdateTwoRuntime atomic.Bool
+
 // OrderWorkflow is the main workflow function
 func OrderWorkflow(ctx tempolite.WorkflowContext[identifier], orderID string) error {
-	total, err := calculateTotal(ctx, orderID)
+	var err error
+	var total float64
+
+	if codeAfterUpdateOneRuntime.Load() { // on purpose to create non-deterministic behavior
+		version := ctx.GetVersion(ChangeIDCalculateTax, tempolite.DefaultVersion, 1)
+		if version == tempolite.DefaultVersion {
+			err = ctx.ExecuteActivityFunc("taxes", ActivityComputeTaxes, orderID).Get(&total)
+			fmt.Println("Using original logic after update: total without tax.")
+		} else {
+			err = ctx.ExecuteActivityFunc("taxes", ActivityComputeTotalWithTax, orderID, total).Get(&total)
+			fmt.Println("Using new logic: total with tax.")
+		}
+	} else {
+		err = ctx.ExecuteActivityFunc("taxes", ActivityComputeTaxes, orderID).Get(&total)
+		fmt.Println("Using original logic before update: total without tax.")
+	}
+
 	if err != nil {
 		return err
 	}
@@ -33,24 +54,15 @@ func OrderWorkflow(ctx tempolite.WorkflowContext[identifier], orderID string) er
 	return nil
 }
 
-func calculateTotal(ctx tempolite.WorkflowContext[identifier], orderID string) (float64, error) {
-	// Use GetVersion to manage changes in the workflow logic
-	version := ctx.GetVersion(ChangeIDCalculateTax, tempolite.DefaultVersion, getMaxSupportedVersion())
-	var total float64
+func ActivityComputeTaxes(ctx tempolite.ActivityContext[identifier], orderID string) (float64, error) {
+	return getOrderSubtotal(orderID), nil
+}
 
-	if version == tempolite.DefaultVersion {
-		// Original logic: total without tax
-		total = getOrderSubtotal(orderID)
-		fmt.Println("Using original logic: total without tax.")
-	} else {
-		// New logic: total with tax
-		subtotal := getOrderSubtotal(orderID)
-		tax := calculateTax(subtotal)
-		total = subtotal + tax
-		fmt.Println("Using new logic: total with tax.")
-	}
+func ActivityComputeTotalWithTax(ctx tempolite.ActivityContext[identifier], orderID string, subtotal float64) (float64, error) {
+	subtotal = getOrderSubtotal(orderID)
+	tax := calculateTax(subtotal)
+	return subtotal + tax, nil
 
-	return total, nil
 }
 
 func getOrderSubtotal(orderID string) float64 {
@@ -63,13 +75,6 @@ func calculateTax(subtotal float64) float64 {
 	return subtotal * 0.10
 }
 
-// getMaxSupportedVersion simulates updating the version in code
-var currentMaxSupportedVersion int = tempolite.DefaultVersion
-
-func getMaxSupportedVersion() int {
-	return currentMaxSupportedVersion
-}
-
 func main() {
 	// Create a new Tempolite instance with a destructive option to reset the database
 	ctx := context.Background()
@@ -78,6 +83,10 @@ func main() {
 		log.Fatalf("Failed to create Tempolite instance: %v", err)
 	}
 	defer tp.Close()
+
+	if err := tp.RegisterActivityFunc(ActivityComputeTaxes); err != nil {
+		log.Fatalf("Failed to register activity: %v", err)
+	}
 
 	// Register the workflow
 	if err := tp.RegisterWorkflow(OrderWorkflow); err != nil {
@@ -95,10 +104,39 @@ func main() {
 		log.Fatalf("Error waiting for workflows to complete: %v", err)
 	}
 
+	orderID1 = "order124"
+	tp.Workflow(ComputeTaxes, OrderWorkflow, orderID1) // on purpose, it won't be scheduled, but the next instance will pick it up
+
+	tp.Close()
+
+	codeAfterUpdateOneRuntime.Store(true) // simulate deploying new code
+
+	<-time.After(1 * time.Second)
+
 	// Simulate updating the version in the code
 	// For example, change the maxSupported version for the ChangeID
 	fmt.Println("\n--- Updating workflow logic to include tax ---\n")
-	currentMaxSupportedVersion = 1
+
+	ctx = context.Background()
+	tp, err = tempolite.New[identifier](ctx)
+	if err != nil {
+		log.Fatalf("Failed to create Tempolite instance: %v", err)
+	}
+	defer tp.Close()
+
+	if err := tp.RegisterActivityFunc(ActivityComputeTaxes); err != nil {
+		log.Fatalf("Failed to register activity: %v", err)
+	}
+
+	// new activity to support
+	if err := tp.RegisterActivityFunc(ActivityComputeTotalWithTax); err != nil {
+		log.Fatalf("Failed to register activity: %v", err)
+	}
+
+	// Register the workflow
+	if err := tp.RegisterWorkflow(OrderWorkflow); err != nil {
+		log.Fatalf("Failed to register workflow: %v", err)
+	}
 
 	// Enqueue the workflow after updating the version
 	orderID2 := "order456"
