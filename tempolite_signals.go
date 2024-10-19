988ae1d877c63fb83728981a18350c83e26c9902
David Roman <romandavid83@gmail.com>
2024-10-19 15:46:04 -0400

    Update tempolite_signals.go

--- tempolite_signals.go
+++ tempolite_signals.go
@@ -17,6 +17,8 @@ func (tp *Tempolite[T]) PublishSignal(workflowID WorkflowID, stepID T, value int
 	ticker := time.NewTicker(time.Second / 16)
 	defer ticker.Stop()
 
+	tp.logger.Debug(tp.ctx, "Publishing to signal", "workflowID", workflowID, "stepID", stepID)
+
 	for {
 		select {
 		case <-tp.ctx.Done():
@@ -34,6 +36,7 @@ func (tp *Tempolite[T]) PublishSignal(workflowID WorkflowID, stepID T, value int
 				if ent.IsNotFound(err) {
 					continue // Signal not created yet, wait for next tick
 				}
+				tp.logger.Error(tp.ctx, "Publish to signal error querying execution relationship", "error", err)
 				return fmt.Errorf("error querying execution relationship: %w", err)
 			}
 
@@ -47,15 +50,16 @@ func (tp *Tempolite[T]) PublishSignal(workflowID WorkflowID, stepID T, value int
 				First(tp.ctx)
 			if err != nil {
 				if ent.IsNotFound(err) {
-					// fmt.Println("Signal not completed yet, waiting for next tick")
 					continue // No completed execution yet, wait for next tick
 				}
+				tp.logger.Error(tp.ctx, "Publish to signal error querying signal execution", "error", err)
 				return fmt.Errorf("error querying signal execution: %w", err)
 			}
 
 			// Encode the value to JSON
 			jsonValue, err := json.Marshal(value)
 			if err != nil {
+				tp.logger.Error(tp.ctx, "Publish to signal error encoding signal value", "error", err)
 				return fmt.Errorf("error encoding signal value: %w", err)
 			}
 
@@ -65,6 +69,7 @@ func (tp *Tempolite[T]) PublishSignal(workflowID WorkflowID, stepID T, value int
 				SetStatus(signalexecution.StatusCompleted).
 				Save(tp.ctx)
 			if err != nil {
+				tp.logger.Error(tp.ctx, "Publish to signal error updating signal execution", "error", err)
 				return fmt.Errorf("error updating signal execution: %w", err)
 			}
 
@@ -74,6 +79,9 @@ func (tp *Tempolite[T]) PublishSignal(workflowID WorkflowID, stepID T, value int
 }
 
 func (tp *Tempolite[T]) enqueueSignal(ctx WorkflowContext[T], stepID T) (SignalID, error) {
+
+	tp.logger.Debug(tp.ctx, "Enqueueing signal", "workflowID", ctx.EntityID(), "stepID", stepID)
+
 	// Check for existing completed signal execution
 	relationship, err := tp.client.ExecutionRelationship.Query().
 		Where(
@@ -93,26 +101,35 @@ func (tp *Tempolite[T]) enqueueSignal(ctx WorkflowContext[T], stepID T) (SignalI
 			WithSignal().
 			First(tp.ctx)
 		if err == nil {
+			tp.logger.Debug(tp.ctx, "Found existing completed signal", "signalID", latestExecution.Edges.Signal.ID)
 			// Found completed execution, return its ID
 			return SignalID(latestExecution.Edges.Signal.ID), nil
 		}
 	} else if !ent.IsNotFound(err) {
+		tp.logger.Error(tp.ctx, "Error checking for existing signal", "error", err)
 		return "", fmt.Errorf("error checking for existing signal: %w", err)
 	}
 
+	tp.logger.Debug(tp.ctx, "Creating new signal transaction")
 	// No existing completed signal, create a new one
 	tx, err := tp.client.Tx(tp.ctx)
 	if err != nil {
+		tp.logger.Error(tp.ctx, "Error creating transaction", "error", err)
 		return "", err
 	}
-	defer tx.Rollback()
 
+	tp.logger.Debug(tp.ctx, "Creating signal entity")
 	signalEntity, err := tx.Signal.Create().
 		SetID(uuid.New().String()).
 		SetStepID(fmt.Sprint(stepID)).
 		SetStatus(signal.StatusPending).
 		Save(tp.ctx)
 	if err != nil {
+		if err := tx.Rollback(); err != nil {
+			tp.logger.Error(tp.ctx, "Error rolling back transaction creating signal entity", "error", err)
+			return "", fmt.Errorf("failed to rollback transaction: %w", err)
+		}
+		tp.logger.Error(tp.ctx, "Error creating signal entity", "error", err)
 		return "", fmt.Errorf("failed to create signal entity: %w", err)
 	}
 
@@ -123,9 +140,15 @@ func (tp *Tempolite[T]) enqueueSignal(ctx WorkflowContext[T], stepID T) (SignalI
 		SetStatus(signalexecution.StatusPending).
 		Save(tp.ctx)
 	if err != nil {
+		if err := tx.Rollback(); err != nil {
+			tp.logger.Error(tp.ctx, "Error rolling back transaction creating signal execution", "error", err)
+			return "", fmt.Errorf("failed to rollback transaction: %w", err)
+		}
+		tp.logger.Error(tp.ctx, "Error creating signal execution", "error", err)
 		return "", fmt.Errorf("failed to create signal execution: %w", err)
 	}
 
+	tp.logger.Debug(tp.ctx, "Creating execution relationship")
 	_, err = tx.ExecutionRelationship.Create().
 		SetRunID(ctx.RunID()).
 		SetParentEntityID(ctx.EntityID()).
@@ -138,12 +161,24 @@ func (tp *Tempolite[T]) enqueueSignal(ctx WorkflowContext[T], stepID T) (SignalI
 		SetChildType(executionrelationship.ChildTypeSignal).
 		Save(tp.ctx)
 	if err != nil {
+		if err := tx.Rollback(); err != nil {
+			tp.logger.Error(tp.ctx, "Error rolling back transaction creating execution relationship", "error", err)
+			return "", fmt.Errorf("failed to rollback transaction: %w", err)
+		}
+		tp.logger.Error(tp.ctx, "Error creating execution relationship", "error", err)
 		return "", fmt.Errorf("failed to create execution relationship: %w", err)
 	}
 
 	if err := tx.Commit(); err != nil {
+		if err := tx.Rollback(); err != nil {
+			tp.logger.Error(tp.ctx, "Error rolling back transaction", "error", err)
+			return "", fmt.Errorf("failed to rollback transaction: %w", err)
+		}
+		tp.logger.Error(tp.ctx, "Error committing transaction", "error", err)
 		return "", fmt.Errorf("failed to commit transaction: %w", err)
 	}
+
+	tp.logger.Debug(tp.ctx, "Singla created", "signalID", signalEntity.ID)
 
 	return SignalID(signalEntity.ID), nil
 }
@@ -164,6 +199,7 @@ func (tp *Tempolite[T]) getSignalInfo(id SignalID, err error) *SignalInfo[T] {
 
 func (s *SignalInfo[T]) Receive(ctx WorkflowContext[T], value interface{}) error {
 	if s.err != nil {
+		s.tp.logger.Error(s.tp.ctx, "Error getting signal info", "error", s.err)
 		return s.err
 	}
 
@@ -173,6 +209,7 @@ func (s *SignalInfo[T]) Receive(ctx WorkflowContext[T], value interface{}) error
 	for {
 		select {
 		case <-ctx.tp.ctx.Done():
+			s.tp.logger.Debug(ctx.tp.ctx, "signal info receive context done")
 			return ctx.tp.ctx.Err()
 		case <-ticker.C:
 			signalExecution, err := s.tp.client.SignalExecution.Query().
@@ -186,28 +223,33 @@ func (s *SignalInfo[T]) Receive(ctx WorkflowContext[T], value interface{}) error
 				if ent.IsNotFound(err) {
 					continue // No completed execution yet, wait for next tick
 				}
+				s.tp.logger.Error(ctx.tp.ctx, "Error querying signal execution", "error", err)
 				return fmt.Errorf("error querying signal execution: %w", err)
 			}
 
 			// Check if value is a pointer
 			valueType := reflect.TypeOf(value)
 			if valueType.Kind() != reflect.Ptr {
+				s.tp.logger.Error(ctx.tp.ctx, "Signal info value must be a pointer")
 				return fmt.Errorf("value must be a pointer")
 			}
 
 			// Check if the output slice is empty
 			if len(signalExecution.Output) == 0 {
+				s.tp.logger.Error(ctx.tp.ctx, "Signal execution output is empty")
 				return fmt.Errorf("signal execution output is empty")
 			}
 
 			// Get the JSON-encoded output value
 			jsonValue, ok := signalExecution.Output[0].(string)
 			if !ok {
+				s.tp.logger.Error(ctx.tp.ctx, "Signal info unexpected output type", "type", reflect.TypeOf(signalExecution.Output[0]))
 				return fmt.Errorf("unexpected output type: expected string, got %T", signalExecution.Output[0])
 			}
 
 			// Decode the JSON value into the provided pointer
 			if err := json.Unmarshal([]byte(jsonValue), value); err != nil {
+				s.tp.logger.Error(ctx.tp.ctx, "Signal info error decoding signal value", "error", err)
 				return fmt.Errorf("error decoding signal value: %w", err)
 			}
 
@@ -217,6 +259,7 @@ func (s *SignalInfo[T]) Receive(ctx WorkflowContext[T], value interface{}) error
 }
 
 func (w *WorkflowContext[T]) Signal(stepID T) *SignalInfo[T] {
+	w.tp.logger.Debug(w.tp.ctx, "Enqueueing signal", "workflowID", w.EntityID(), "stepID", stepID)
 	id, err := w.tp.enqueueSignal(*w, stepID)
 	return w.tp.getSignalInfo(id, err)
 }
